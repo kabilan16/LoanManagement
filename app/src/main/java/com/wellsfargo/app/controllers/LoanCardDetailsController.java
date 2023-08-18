d4f1eacc7c0de94c93cd212ac3a23ac3fba69ad3
AvaneSauhard <sauhardpareek@gmail.com>
2023-08-18 15:28:45 +0530

    loancarddetails

--- app/src/main/java/com/wellsfargo/app/controllers/LoanCardDetailsController.java
+++ app/src/main/java/com/wellsfargo/app/controllers/LoanCardDetailsController.java
@@ -1,8 +1,11 @@
 package com.wellsfargo.app.controllers;
 
+import com.wellsfargo.app.entities.EmployeeMaster;
 import com.wellsfargo.app.entities.LoanCardDetails;
 import com.wellsfargo.app.services.LoanCardDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,16 +13,27 @@ import org.springframework.web.bind.annotation.*;
 public class LoanCardDetailsController {
     @Autowired
     private LoanCardDetailsService loanCardDetailsService;
-
-    @PostMapping("/addLoanCard")
-    public LoanCardDetails addLoanCard(@RequestBody LoanCardDetails loanCardDetails){
-        return loanCardDetailsService.addLoanCard(loanCardDetails);
+    
+    @GetMapping("/getLoanDetails/{id}")
+    public ResponseEntity<LoanCardDetails> getLoanCardDetails(@PathVariable Integer id){
+    	return ResponseEntity.ok(loanCardDetailsService.findLoanCardDetailsById(id).get());
+    }
+    @PostMapping("/addLoanDetails")
+    public ResponseEntity<LoanCardDetails> createLoanCardDetails(@RequestBody LoanCardDetails loan ){
+    	LoanCardDetails savedLoan = loanCardDetailsService.createLoanCardInfo(loan);
+		return new ResponseEntity<>(savedLoan, HttpStatus.CREATED);
+    }
+    @PutMapping("/updateLoanDetails")
+    public ResponseEntity<LoanCardDetails> updateLoanardDetails(@RequestBody LoanCardDetails loan)
+    {
+    	LoanCardDetails updatedLoan = loanCardDetailsService.updateLoanCardDetails(loan);
+    	return new ResponseEntity<>(updatedLoan, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/deleteLoanDetails/{id}")
+    public ResponseEntity<String> deleteDetailsById(@PathVariable Integer id)
+    { 
+    	return ResponseEntity.ok(loanCardDetailsService.deleteDetailsById(id));
     }
 
-    @PostMapping("/getLoanCardByLoanType")
-    @ResponseBody
-    public LoanCardDetails getLoanCardByLoanType(@RequestBody LoanCardDetails loanCardDetails){
-        return loanCardDetailsService.getLoanCardByLoanType(loanCardDetails.getLoanType());
-    }
-
+ 
 }
--- app/src/main/java/com/wellsfargo/app/services/LoanCardDetailsService.java
+++ app/src/main/java/com/wellsfargo/app/services/LoanCardDetailsService.java
@@ -1,7 +1,43 @@
 package com.wellsfargo.app.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.wellsfargo.app.repositories.LoanCardDetailsRepo;
+import com.wellsfargo.app.entities.LoanCardDetails;
 @Service
 public class LoanCardDetailsService {
+	@Autowired
+	private LoanCardDetailsRepo loanCardDetailsRepo;
+
+	public Optional<LoanCardDetails> findLoanCardDetailsById(Integer id) {
+		// TODO Auto-generated method stub
+		return loanCardDetailsRepo.findById(id);
+	}
+
+	public LoanCardDetails createLoanCardInfo(LoanCardDetails loan) {
+		// TODO Auto-generated method stub
+		return loanCardDetailsRepo.save(loan);
+	}
+
+	public LoanCardDetails updateLoanCardDetails(LoanCardDetails loan) {
+		// TODO Auto-generated method stub
+		LoanCardDetails details = loanCardDetailsRepo.getReferenceById(loan.getLoanId());
+		details.setDurationInMonths(loan.getDurationInMonths());
+		return loanCardDetailsRepo.save(details);
+		
+
+	}
+
+	public String deleteDetailsById(Integer id) {
+		// TODO Auto-generated method stub
+		loanCardDetailsRepo.deleteById(id);
+		return "Deleted Successfully!";
+	}
+	
+	
+	
 }

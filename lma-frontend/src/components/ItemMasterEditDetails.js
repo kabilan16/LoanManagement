74b7b2cb56658e9ac37475b5c7e7dd9b241c0fdf
yashaswiniwells <yashaswinisharma3@gmail.com>
2023-08-21 15:26:12 +0530

    changes

--- lma-frontend/src/components/ItemMasterEditDetails.js
+++ lma-frontend/src/components/ItemMasterEditDetails.js
@@ -1,7 +1,148 @@
 import "../LoginPage.css";
 
-import React from "react";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Button, Table } from "react-bootstrap";
+import { Modal, Paper } from '@mui/material';
+
+function LoanDataEdit() {
+  const [employees, setEmployees] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editEmployee, setEditEmployee] = useState(null);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [deletedEmployeeId, setDeletedEmployeeId] = useState(null);
+
+  useEffect(() => {
+    
+    const delay = 500;
+    setTimeout(() => {
+      axios.get('http://localhost:8081/getAllItem')
+        .then(response => {
+          console.log('Fetched data:', response.data);
+          setEmployees(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }, delay);
+  }, ["no response "]);
+
+  const handleEdit = (employee) => {
+    setIsEditing(true);
+    setEditEmployee(employee);
+  };
+
+  const handleEditSubmit = async (editedEmployee) => {
+    try {
+      await axios.put(`http://localhost:8081/updateItem`, editedEmployee);
+      const updatedEmployees = employees.map(emp =>
+        emp.itemID === editedEmployee.itemId ? editedEmployee : emp
+      );
+      setEmployees(updatedEmployees);
+      setIsEditing(false);
+      setEditEmployee(null);
+      // Simulate a refresh effect for 700ms
+      setTimeout(() => {
+        setIsEditing(false);
+      }, 700);
+    } catch (error) {
+      console.error('Error updating items:', error);
+    }
+  };
+
+  const handleDelete = async (employeeId) => {
+    setIsDeleting(true);
+    setDeletedEmployeeId(employeeId);
+    try {
+      await axios.delete(`http://localhost:8081/deleteItemDetails/${employeeId}`);
+      setEmployees(employees.filter(emp => emp.itemId !== employeeId));
+      // Simulate a refresh effect for 700ms
+      setTimeout(() => {
+        setIsDeleting(false);
+      }, 700);
+    } catch (error) {
+      console.error('Error deleting items:', error);
+      setIsDeleting(false);
+    }
+  };
+
+  const EditForm = ({ employee, onClose, onSubmit }) => {
+    const [formData, setFormData] = useState(employee);
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        await axios.put(`http://localhost:8081/updateItem`, formData);
+        onSubmit(formData); // Notify the parent component about the update
+        onClose(); // Close the edit form
+      } catch (error) {
+        console.error('Error updating item:', error);
+      }
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Item ID:</label>
+            <input
+              type="number"
+              name="itemId"
+              value={formData.itemId}
+              onChange={handleInputChange}
+              readOnly // To prevent editing the ID
+            />
+          </div>
+          <div>
+            <label>Item Description:</label>
+            <input
+              type="text"
+              name="itemDesc"
+              value={formData.itemDesc}
+              onChange={handleInputChange}
+              readOnly // To prevent editing the ID
+            />
+          </div>
+          <div>
+          <label>
+                      Issue Status
+                        <select 
+                        name="issueStatus"
+                        value={formData.issueStatus}
+          onChange={handleInputChange}>
+                        <option value="" selected disabled hidden>Select status</option>
+                          <option value="Yes">Yes</option>
+                          <option value="No">No</option>
+
+          </select>
+          </label> 
+          </div>
+          <div>
+            <label>Loan Duration in months:</label>
+            <input
+              type="number"
+              name="durationInMonths"
+              value={formData.durationInMonths}
+              onChange={handleInputChange}
+            />
+          </div>
+                    <br></br>
+          <button type="submit" style={{marginRight: "5px"}}>Submit</button>
+          <button onClick={onClose}>Cancel</button>
+        </form>
+      </div>
+    );
+  };
+} 
 function ItemMasterEditDetails() {
   return (
     <div>

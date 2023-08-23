acfbca2f70d6ea4a61f2f6488a41e8edea219e48
Kabilan <mkkabil2002@gmail.com>
2023-08-23 18:24:40 +0530

    frontend changes done

--- lma-frontend/src/components/ViewLoans.js
+++ lma-frontend/src/components/ViewLoans.js
@@ -1,20 +1,42 @@
 import "../LoginPage.css";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-import React from "react";
 import { Table } from "react-bootstrap";
 function ViewLoans() {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    
+    const delay = 500;
+    setTimeout(() => {
+      axios.get('http://localhost:8081/getAllLoanDetails/21')
+        .then(response => {
+          console.log('Fetched data:', response.data);
+          setEmployees(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }, delay);
+  }, ["no response "]);
   return (
     <div>
       <center>
         <h3 className="pagetitle">Loan Cards Availed</h3>
       </center>
-      <center>
-        <div className="userDetails">
-          <p>Employee id: xyz</p>
-          <p>Designation: xyz</p>
-          <p>Department: xyz</p>
-        </div>
+          {employees.map(emp => (
+           <center>
+           <div className="userDetails"> 
+          <p key={emp.employeeId}>Employee ID: {emp.employeeId}</p>
+          <p key={emp.employeeId}>Designation: {emp.designation}</p>
+          <p key={emp.employeeId}>Department: {emp.department}</p>
+          </div>
       </center>
+          
+        ))}
+
+        
+        
       <Table bordered hover responsive className="custom-table">
         <thead>
           <tr>
@@ -26,17 +48,20 @@ function ViewLoans() {
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>John</td>
+            <td>30</td>
+            <td>Dummy data</td>
             <td>5</td>
             <td>12/01/2023</td>
           </tr>
-          <tr>
-            <td>2</td>
-            <td>Jane</td>
-            <td>6</td>
-            <td>28/03/2023</td>
+          {employees.map(emp => (
+            <tr>
+          <td key={emp.loanId}>{emp.loanId}</td>
+          <td key={emp.loanId}>{emp.loanType}</td>
+          <td key={emp.loanId}>{emp.durationInMonths}</td>
+          <td key={emp.loanId}>{emp.cardIssueDate}</td>
+          
           </tr>
+        ))}
           {/* Add more rows as needed */}
         </tbody>
       </Table>
